4548705ccde1043a193e9db4df8c415834368b74
khushboo545567 <khushbooyadav6266763530@gmail.com>
2026-08-09 10:01:55 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import Sidebar from "./components/Sidebar";
-// import Dashboard from "./pages/Dashboard";
-// import Products from "./pages/Products";
-// import Orders from "./pages/Ordres";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-
-// function App() {
-//   return (
-//     <div className="flex min-h-screen">
-//       <Sidebar />
-
-//       <main className="flex-1 p-6">
-//         <Routes>
-//           <Route path="/" element={<Dashboard />} />
-//           <Route path="/products" element={<Products />} />
-//           <Route path="/orders" element={<Orders />} />
-//         </Routes>
-
-//         <ToastContainer
-//           position="top-right"
-//           autoClose={2000}
-//           hideProgressBar={false}
-//           newestOnTop
-//           closeOnClick
-//           pauseOnHover
-//         />
-//       </main>
-//     </div>
-//   );
-// }
-// export default App;
-
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
