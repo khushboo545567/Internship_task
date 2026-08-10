fea5c6b3d698d93105c04f119a6dd21b129e0921
khushboo545567 <khushbooyadav6266763530@gmail.com>
2026-08-10 23:59:14 +0530

    Fix MongoDB connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
